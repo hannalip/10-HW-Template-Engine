c281877e851a4d0e060944851e8004a051ff13ec
Hannali Patidar <hannalip@gmail.com>
2020-02-05 17:19:40 -0500

    added to app file, worked on prompts for the user in node.

--- app.js
+++ app.js
@@ -1,33 +1,138 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const Manager = require("./lib/manager");
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Inquirer = require('inquirer');
+const fs = require('fs');
 
-const managerQuestion = [
-    {
-        type: "input",
-        name: "name",
-        message: "Enter manger name:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter manger's ID:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter manger's email:",
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Enter manger's office number:",
-    },
-    {
-        type: "list",
-        name: "hasTeam",
-        message: "Do you have any team members?",
-        choices: ["Yes", 
-        "No"]
+var teamMembers = [];
+
+//Create Objects of team members
+const appMenu = async () => {
+    // Create Manager object
+    const createManager = async () => {
+        const answer = await Inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter your name',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Enter your id',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Enter your email',
+            },
+            {
+                type: 'input',
+                name: 'officeNum',
+                message: 'Enter your office number',
+            }
+        ]);
+        const manager = await new Manager(
+            this.name,
+            this.id,
+            this.email,
+            this.officeNum
+        );
+        await teamMembers.push(manager);
+        console.log('team members', teamMembers);
+    };
+
+    //Prompt for new types of employees
+    const createTeam = async () => {
+        const prompt = await Inquirer.prompt({
+            type: 'list',
+            name: 'member',
+            message: 'What type of team member do you want to add?',
+            choices: ['Engineer', 'Intern', "I'm done"],
+  
+        });
+
+        if (prompt.member === 'Engineer') {
+            await createEngineer();
+            await createTeam();
+        } else if (prompt.member === 'Intern') {
+            await createIntern();
+            await createTeam();
+        } else {
+            //Return Array of team member objects
+            return teamMembers;
+        }
+    };
+    //Function to create intern object
+    async function createIntern() {
+        const answer = await Inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter your name',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'Enter your id',
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: 'Enter your email',
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: 'Enter your school',
+                name: 'school'
+            }
+        ]);
+        const newIntern = await new Intern(
+            this.name,
+            this.id,
+            this.email,
+            this.school
+        );
+
+        await teamMembers.push(newIntern);
     }
-]
+    //Function to create Engineer Object
+    async function createEngineer() {
+        const answer = await Inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter your name',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'Enter your id',
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: 'Enter your email',
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: 'Enter your github',
+                name: 'github'
+            }
+        ]);
+        const newEngineer = await new Engineer(
+            this.name,
+            this.id,
+            this.email,
+            this.github
+        );
+
+        await teamMembers.push(newEngineer);
+    }
+
+    await createManager();
+    await createTeam();
+};
+
+appMenu();
